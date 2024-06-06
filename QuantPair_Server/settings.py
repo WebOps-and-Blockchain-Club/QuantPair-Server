c85e4c11e78e4b2da161d882372b8ac7ddf947d4
Ovetsarilish <ovetsarilish@gmail.com>
2024-06-07 00:05:25 +0530

    feat : Added Johansen Test as an application :sparkles:

--- QuantPair_Server/settings.py
+++ QuantPair_Server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Registering our application in the project
+    'JohansenTest',
 ]
 
 MIDDLEWARE = [
