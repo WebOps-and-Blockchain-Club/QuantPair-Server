bed6f62692629d4ea69f5a1e8eaa24ce8de2220a
Ovetsarilish <ovetsarilish@gmail.com>
2024-09-02 17:41:56 +0530

    docs : Comments added and some refactoring :package:

--- WishList/serializers.py
+++ WishList/serializers.py
@@ -2,18 +2,24 @@ from rest_framework import serializers
 from .models import UserWishList_Pair
 
 class UserWishList_PairSerializer(serializers.ModelSerializer) :
-
+    # we need all fields in our serializer to take
     class Meta : 
+        # referencing the model for wishlist in metadata
         model = UserWishList_Pair
-
+        # all fields included
         fields = '__all__'
     
+    # to create a new wishlist item
     def create(self, validated_data) : 
+        # getting data from user and assigning into variables for better reading
         symbol1 = validated_data['symbol1']
         symbol2 = validated_data['symbol2']
+        # here user is primary key of user, which is the reference to the user
         user = validated_data['user']
+        # creating the wishlist item
         wishlist_item = UserWishList_Pair(symbol1=symbol1,
                                            symbol2=symbol2, 
                                            user=user)
+        # savin it and returning
         wishlist_item.save()
         return wishlist_item
--- WishList/urls.py
+++ WishList/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # making endpoints for each operation 
     path('getWishList', views.getUserWishlist, name='getWishList'),
     path('createWishList', views.createWishList, name='createWishList'),
     path('deleteWishList', views.deleteWishList, name='deleteWishList'),
